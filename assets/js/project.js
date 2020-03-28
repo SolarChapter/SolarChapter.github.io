04fae935334123e3f126f97945a93d1fb1191853
Kelvien Hidayat <kelvien.hidayat@gmail.com>
2020-03-28 11:02:43 -0400

    Touch up

--- assets/js/project.js
+++ assets/js/project.js
@@ -1,6 +1,15 @@
+const booksTabContainerPaddingTop = parseInt($(".books-tab-container").css("padding-top"));
+const tabContentPaddingTop = parseInt($(".tab-content").css("padding-top"));
+
+
 $("a[data-toggle='tab']").on('shown.bs.tab', function (e) {
   const iframe = $(e.relatedTarget.hash).find("iframe");
   const src = iframe.attr("src");
   iframe.attr("src", "");
   iframe.attr("src", src);
+
+  // Scroll tab content to top
+  $(window).scrollTop(
+    $(".tab-content").offset().top - tabContentPaddingTop - booksTabContainerPaddingTop
+  );
 });
